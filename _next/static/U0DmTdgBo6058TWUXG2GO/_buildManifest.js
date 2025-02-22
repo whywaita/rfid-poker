55f50eb5a5008d55d7bac63b12d176b6833338e9
whywaita <whywaita@users.noreply.github.com>
2025-02-22 05:59:37 +0000

    deploy: ae892d8bd7c88fae33f4431f18a5d5d528d36623

--- _next/static/U0DmTdgBo6058TWUXG2GO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/424-c889e852664f8faa.js","static/chunks/pages/index-f08d236391d310c7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
